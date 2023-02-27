6cece705da8ff62e00ba422326073b0e18b30d28
justinrespet <62667760+justinrespet@users.noreply.github.com>
2023-02-27 12:41:50 -0700

    Working on connectivity program for testing

--- picoInternet.py
+++ picoInternet.py
@@ -1,1 +1,21 @@
+# 
+# Pico internet connection protocol 
+# February 27, 2023
+#
+# Sources:
+# projects.raspberrypi.org
 
+import network
+import socket
+from time import sleep
+from picozero import pico_temp_sensor, pico_led
+import machine
+
+ssid = "Name of wifi network"
+password = "Wifi network password"
+
+def connect():
+    #connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
