90f2c9d56dc136a5742478152976cadb1a929d6b
johannashaw <77993857+johannashaw@users.noreply.github.com>
2023-03-01 11:49:54 -0700

    Got proof of life for I2C VEML7700

--- I2C_Classes.py
+++ I2C_Classes.py
@@ -1,10 +1,55 @@
-# This will contain all of the I2C classes
-# Separate classes for light sensor, colour sensor, and maybe lights (if it takes I2C)
+# Project: liteCTRL
+# File: Custom I2C classes. Contains class defs for the following:
+#           - VEML7700 (ambient light sensor)
+#           - APDS9960 (light and light temp sensor)
+#           - LED_Strip (maybe, depends if it needs I2C)
+# Created by: Johanna Shaw
+from machine import Pin, I2C, SoftI2C
+
 
 # the ambient light sensor
 class VEML7700:
     # Attributes:
     
-    def __init__(self):
+    # Note:
+    #   Light level [lx] is (ALS OUTPUT DATA [dec.] / ALS Gain x responsivity)
+    def __init__(self, SCL, SDA):
         # not sure if this is necessary yet but I'm putting it here anyway
+
+        # 000                   -- reserved
+        #    00                 -- gain of x1, starting simple
+        #      0                -- Res
+        #       000             -- Integration time, starting at 100ms (idk)
+        #          ??           -- ALS persistence (no idea)
+        #            00         -- Res
+        #              0        -- Interrupt disabled
+        #               0       -- ALS power on
+
+        # initializes the I2C channels
+        self.i2c = I2C(id=0, scl=Pin(SCL, mode=Pin.ALT), sda=Pin(SDA, mode=Pin.ALT),  freq=400000)
+        
+        if 16 not in self.i2c.scan():
+            print('VEML7700 is not responding')
+
+    
+    # Formats the info given so that you can write to the device easily
+    def I2C_Write(self, Cmd_code):
+
+        # VEML7700 write I2C formatting:
+        #   Start condition
+        self.i2c.start()
+        #   Send Device address
+        #   Write bit (0)
+        self.i2c.write(20)  # address is 16, tehn the write bit
+        #   ACK
+        #   Command code
+        #   ACK
+        #   LSB Data
+        #   ACK
+        #   MSB Data
+        #   ACK
+        #   Stop Condition
+
         pass
+
+    
--- main.py
+++ main.py
@@ -19,8 +19,20 @@ timrr = Timer()
 def Main():
     global ourMotor, MGoBrr, timrr
 
-    print("First")
+    print("Got to main")
 
+    # MotorTesting()
+
+    # TESTING I2C:
+    # initialize the I2C channels
+    # GPIO pins 4 and 5 map to irl pins 6 and 7
+    i2c = I2C(id=0, scl=Pin(5, mode=Pin.ALT), sda=Pin(4, mode=Pin.ALT),  freq=400000)
+    
+    print(i2c.scan())
+    print('done scanning')
+
+
+def MotorTesting():       
     # lets go for pins [24:27] (gp 18:21)
     # enable pin is on GPIO 28, or pin 34 irl
     ourMotor = Motor(18, 19, 20, 21, 28)
@@ -29,15 +41,8 @@ def Main():
 
     #MGoBrr.irq(handler=m_OnOff, wake=Pin.IDLE)#, trigger=Pin.IRQ_RISING)#, wake=machine.IDLE|machine.SLEEP)
 
+    # Timer for testing start/stop functionality of the motor.
     timrr.init(freq=1, mode=Timer.PERIODIC, callback=m_OnOff)
-
-
-    # TESTING I2C:
-    # initialize the I2C channels
-    #i2c = SoftI2C(Pin(5), Pin(4))
-    
-    #print(i2c.scan())
-
 
 # Used for testing motor library. Toggles motor between on and off using the enable pin.
 def m_OnOff(PIN):
