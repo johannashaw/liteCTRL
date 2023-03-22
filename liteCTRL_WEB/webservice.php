ea02d53e8d086a1f39771af5e86689af6ae51605
Justin Respet <justinrespet@gmail.com>
2023-03-22 10:37:41 -0600

    webservice has been refactored

--- liteCTRL_WEB/webservice.php
+++ liteCTRL_WEB/webservice.php
@@ -1,58 +1,75 @@
 <?php
 
-
 require_once "database.php";
 
-// --- Initial Test Code ------------------------------------------------------------------------
-
-if (isset($_GET['device']) && $_GET['device'] == 'motor')
-{
-    error_log("Show me a database update");
-
-    if(isset($_GET['action']) && $_GET['action'] == 'GoBrr')
-    {
-        $randomNumber = random_int(0, 10);
-        $query = "UPDATE Motor";
-        $query .= " SET Operate = '$randomNumber'";
-        
-
-        $results = mysqlNonQuery( $query );
-    }
-}
-
-if (isset($_GET))
-{
-    error_log("Basic GET request");
-}
-
-error_log(json_encode(($_GET)));
 
 // --- PICO Webservice -----------------------------------------------------------------------
 
 // The webservice for the Pico will mostly involve regularly querying the database to 
 // determing if any user setting has been changed, and setting control parameters back 
-// to the  resting state. If the Pico is in automatic mode it will have no need to 
+// to the resting state. If the Pico is in automatic mode it will have no need to 
 // interact with database at all, other than logging data
 
 // Pico Check In
+// Query the database control fields to determine if user has indicated any operations
 
-if(isset($_GET["checkin"]))
+if(isset($_GET["CheckIn"]))
 {
-    GetData();
-    //echo "checked";
+    CheckIn();
+}
+
+function CheckIn()
+{
+    // $query = "SELECT *";
+    // $query .= " FROM MotorControl";
+
+    // $results = mySQLQuery( $query );
+
+    // if ($results)
+    // {
+    //     while ( $row = $results->fetch_assoc())
+    //     {
+    //         $returnArray[] = $row;
+    //     }
+
+    //     echo json_encode($returnArray);
+    // }
+    // else
+    // {
+    //     echo "GetData Error";
+    // }
 }
 
 // Pico Control Field Resets
 
-// Exterior Light Intensity Log
+if(isset($_GET["Reset"]))
+{
+    Reset();
+}
 
-// Exterior Light Temperature Log
+function Reset()
+{
 
-// Interior Light Intensity Log
+}
 
-// Interior Light Intensity Log
+// Log Data
+// Hourly logging of data to database, including:
+// Exterior Light Intensity
+// Exterior Light Temperature
+// Interior Light Intensity
+// Interior Light Temperature
+// Curtain Position
 
-// Curtain Position Log
+if(isset($_GET["LogData"]))
+{
+    LogData();
+}
+
+function LogData()
+{
+
+}
+
 
 
 // --- WEBSITE Webservice --------------------------------------------------------------------
@@ -66,28 +83,6 @@ if(isset($_GET["checkin"]))
 // LED
 
 
-// --- Database Operations -------------------------------------------------------------------
 
-function GetData()
-{
-    $query = "SELECT *";
-    $query .= " FROM MotorControl";
-
-    $results = mySQLQuery( $query );
-
-    if ($results)
-    {
-        while ( $row = $results->fetch_assoc())
-        {
-            $returnArray[] = $row;
-        }
-
-        echo json_encode($returnArray);
-    }
-    else
-    {
-        echo "GetData Error";
-    }
-}
 
 ?>
