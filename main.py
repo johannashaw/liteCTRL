191e1252d697356394f7ada0e9cb7bee68f23493
johannashaw <77993857+johannashaw@users.noreply.github.com>
2023-04-04 14:51:01 -0600

    Calibrate is Working :)

--- main.py
+++ main.py
@@ -54,32 +54,24 @@ class Main:
         Backward = Pin(15, Pin.IN)
         Backward.irq(handler=self.CloseCallback, trigger=Pin.IRQ_RISING)
 
-    def OpenCallback(self, pin):
+    def OpenCallback(self, pin):           
+        # GPIO pin 16
         print('openning')
         self.ourMotor.Open()
         
     def CloseCallback(self, pin):
+        # GPIO piin 15
         print('closing')
         self.ourMotor.Close()
 
-    def ADCCallback(self, timer):        
-        print(self.BarPin.read_u16())
-
-    # testing a strip that runs off of PWM
-    def PWM_StripTest(self):
+    # Initializes the light strip object
+    # This should be the only add on that doesn't risk failing
+    def PWM_Strip_Init(self):
 
         # initialize strip
         # GPIO pins: 11, 12, 13  =  irl pin: 15, 16, 17
-        self.Strip = LED_Strip_PWM(11, 12, 13)
+        self.Strip = LED_Strip_PWM(R_Pin=11, G_Pin=12, B_Pin=13)
         print('Light strip initialized')
-
-        # pass a couple of different colour values
-        #self.Strip.Set_Colour(Colour(255, 255, 255))
-        self.Strip.Set_Colour(Colour(255, 0, 0))
-        # self.Strip.Set_Colour(Colour())
-        # self.Strip.Set_Colour(Colour())
-        # self.Strip.Set_Colour(Colour())
-        print('Colour Changed')
 
       
     def MotorInit(self):       
@@ -87,6 +79,8 @@ class Main:
         # enable pin is on GPIO 10, or pin 14 irl
         # ADCBarrier pin is on GPIO 28, or pin 34 irl
         self.ourMotor = Motor(18, 19, 20, 21, pinEnable=10, pinADCBarrier=28)
+
+        self.ourMotor.Frequency = 750
 
         self.ourMotor.Calibrate()
 
@@ -127,8 +121,6 @@ class Main:
             self.APDS = None
             print(err)
             return
-        finally:
-            pass
         
 
     def SensorDataCallback(self, PIN):
@@ -142,33 +134,6 @@ class Main:
 
         
         print(f'lux = {lux}, clear ={clear}, red = {red}, green = {green}, blue = {blue}')
-
-    # This tests the LED strip
-    def WS2812B_Strip_Test(self):
-        
-        # initialize the light strip object
-        # GPIO in 13, maps to irl pin 17
-        self.strip = WS2812B_Strip(13, 1)
-
-        # # create the array of lights
-        # lights = []
-        # for i in range(60):
-        #     lights.append(Colour(109, 0, 162))
-
-        self.strip.SetColours(109, 0, 162, False)
-
-        time.sleep(1)
-        # send Colours
-        self.strip.SendColours()
-
-        print("Done Light Testing")
-
-        self.timrr.init(freq=1, mode=Timer.PERIODIC, callback=self.ColourSendTest)
-
-    
-    def ColourSendTest(self, PIN):
-        
-        self.strip.SendColours()
         
 
     # Callback for VEML testing, prints LUX
@@ -178,18 +143,6 @@ class Main:
 
 
 
-    # Used for testing motor library. Toggles motor between on and off using the enable pin.
-    def m_OnOff(self, PIN):
-        print("I'm in.")
-
-        if self.ourMotor.Started:
-            self.ourMotor.Stop()
-        else:
-            # ourMotor.StartForward(500)
-            self.ourMotor.StartBackward(500)
-
-
 
 if __name__ == '__main__':
     Main()
-0
